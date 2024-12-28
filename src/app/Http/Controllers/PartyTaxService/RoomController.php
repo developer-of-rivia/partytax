1c7e8e6db94221eb8095a0255b2f0e34a6ce4727
developer-of-rivia <samping2money@gmail.com>
2024-12-29 01:25:25 +0700

    index results расписан комментариями

--- src/app/Http/Controllers/PartyTaxService/RoomController.php
+++ src/app/Http/Controllers/PartyTaxService/RoomController.php
@@ -25,22 +25,37 @@ class RoomController extends Controller
     /* results */
     public function indexResults()
     {
+
+        // получаем всех участников данной комнаты
         $allMembers = RoomMember::where('room_id', session()->get('current_room'))->get();
+        // создаём массив нужных данных для вьюхи
         $allMembersResults = [];
         
+
+        // начинаем по одному выбирать участников комнаты
         foreach($allMembers as $member){
+            // берём все траты у конкретного итерируемого участника комнаты
             $hisExpenses = MemberExpense::where('member_id', $member->id)->get();
+            // указываем начальную сумму скида
             $hisResult = 0;
 
+            // начинаем по подному выбирать траты конкретного итерируемого участника комнаты
             foreach($hisExpenses as $expense){
-                $currentExpense = Expense::where('id', $expense->expense_id)->get()->first()->price;
+                // получаем цену конкретной итерируемой траты
+                $currentExpensePrice = Expense::where('id', $expense->expense_id)->get()->first()->price;
+                // получаем количество людей, который скидываются на данную трату
                 $contributorsCount = MemberExpense::where('expense_id', $expense->expense_id)->get()->count();
-                $contributorsPart = $currentExpense / $contributorsCount;
+                
+                // Делим цену траты на количество скидывающихся
+                $contributorsPart = $currentExpensePrice / $contributorsCount;
+                // приводим к формату до точки
                 $contributorsPart = number_format($contributorsPart);
 
+                // добавляем долю от данной траты в итогую сумму для участника
                 $hisResult = $hisResult + $contributorsPart;
             }
             
+            // наполняем массив нужных данных для вьюхи
             $allMembersResults[$member->name] = $hisResult;
         }
 
